fe9a886c2958fc6f00872d1d5566709d2569ef6a
Rainer Schmied <info@sarplataygemas.com>
2016-10-06 08:48:55 +0200

    Update sage_pay_server.php
    corregodo "parámetros"

--- includes/languages/espanol/modules/payment/sage_pay_server.php
+++ includes/languages/espanol/modules/payment/sage_pay_server.php
@@ -15,7 +15,7 @@
   define('MODULE_PAYMENT_SAGE_PAY_SERVER_TEXT_DESCRIPTION', '<img src="images/icon_info.gif" border="0" />&nbsp;<a href="http://library.oscommerce.com/Package&en&sage_pay&oscom23&server" target="_blank" style="text-decoration: underline; font-weight: bold;">Ver documentación on-line</a><br /><br /><img src="images/icon_popup.gif" border="0">&nbsp;<a href="https://support.sagepay.com/apply/default.aspx?PartnerID=E194E079-84A9-493C-AB9A-91CB362D3238&PromotionCode=osc3MF" target="_blank" style="text-decoration: underline; font-weight: bold;">Sitio web de Sage</a>&nbsp;<a href="javascript:toggleDivBlock(\'sagePayInfo\');">(info)</a><span id="sagePayInfo" style="display: none;"><br /><i>Si utiliza el enlace anterior para suscribirse, Sagepay otorgara a osCommerce una pequeña bonificación económica por referir a un cliente.</i></span>');
 
   define('MODULE_PAYMENT_SAGE_PAY_SERVER_ERROR_ADMIN_CURL', 'Este módulo requiere que cURL esté habilitado en PHP y no funcionará hasta que se haya habilitado en este servidor.');
-  define('MODULE_PAYMENT_SAGE_PAY_SERVER_ERROR_ADMIN_CONFIGURATION', 'Este módulo no funcionará hasta que configure los parametros de Login del Vendedor. Por favor configure los ajustes del módulo.');
+  define('MODULE_PAYMENT_SAGE_PAY_SERVER_ERROR_ADMIN_CONFIGURATION', 'Este módulo no funcionará hasta que configure los parámetros de Login del Vendedor. Por favor configure los ajustes del módulo.');
 
   define('MODULE_PAYMENT_SAGE_PAY_SERVER_ERROR_TITLE', 'Se ha producido un error al procesar su tarjeta de crédito');
   define('MODULE_PAYMENT_SAGE_PAY_SERVER_ERROR_GENERAL', 'Por favor, inténtelo de nuevo y si los problemas persisten, intente otra forma de pago.');
