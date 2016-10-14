424eb53811263f5abb648fb1d76ab544edf51177
Lionel <admin@nukua.es>
2016-10-15 01:05:59 +0200

    Update contact_us.php
    Sale en el campo del nombre ENTRY_NAME_TEXT y queda feo

--- includes/languages/espanol/contact_us.php
+++ includes/languages/espanol/contact_us.php
@@ -16,6 +16,7 @@ define('TEXT_SUCCESS', 'Su consulta ha sido enviada al encargado de la tienda.')
 define('EMAIL_SUBJECT', 'Consulta desde ' . STORE_NAME);
 
 define('ENTRY_NAME', 'Nombre y Apellido:');
+define('ENTRY_NAME_TEXT', '');//Sale en el campo del nombre ENTRY_NAME_TEXT y queda feo
 define('ENTRY_EMAIL', 'Dirección E-Mail:');
 define('ENTRY_ENQUIRY', 'Consulta:');
 define('ENTRY_ENQUIRY_TEXT', '');
