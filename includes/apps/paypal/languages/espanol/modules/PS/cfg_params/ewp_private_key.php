fcb706cdd51040b72c4f72ce8a5aff680ef9b1cb
Lionel <admin@nukua.es>
2017-02-06 15:18:13 +0100

    Update ewp_private_key.php
    spanish

--- includes/apps/paypal/languages/espanol/modules/PS/cfg_params/ewp_private_key.php
+++ includes/apps/paypal/languages/espanol/modules/PS/cfg_params/ewp_private_key.php
@@ -1,2 +1,3 @@
-cfg_ps_ewp_private_key_title = Your Private Key
-cfg_ps_ewp_private_key_desc = The location and filename of your Private Key to use for encrypting the parameters. (*.pem)
+cfg_ps_ewp_private_key_title = Su clave privada
+cfg_ps_ewp_private_key_desc = La ubicación y el nombre de archivo de su clave privada que se usa para encriptar los parámetros. (*.pem)
+
